a8a0ac021493ce74d7abaae8526ada29cdf1b76a
ffaustofortesKK <ffaustofortes@gmail.com>
2026-07-22 16:51:59 +0100

    Update tv.py

--- tv.py
+++ tv.py
@@ -156,7 +156,7 @@ else:
                     .mini-controls {{
                         position: absolute;
                         bottom: 5px;
-                        left: 20px;
+                        left: 50px;
                         right: 5px;
                         background: rgba(0, 0, 0, 0.85);
                         border: 1px solid #ffd700;
